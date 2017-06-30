bef32aad5f8fcba2d6425ef1eadc46750c92838f
Jonatan Kłosko <jonatanklosko@gmail.com>
2017-07-01 00:24:26 +0200

    Add statistic: Most podiums at a single competition.

--- statistics/index.rb
+++ statistics/index.rb
@@ -4,6 +4,7 @@ Dir["#{__dir__}/*.rb"].reject { |path| path.end_with?(__FILE__) }.each { |file| 
 STATISTICS = {
   "most_4th_places" => Most4thPlaces.new,
   "most_finals" => MostFinals.new,
+  "most_podiums_at_single_competition" => MostPodiumsAtSingleCompetition.new,
   "most_podiums_together" => MostPodiumsTogether.new,
   "most_visited_countries" => MostVisitedCountries.new,
 }
---
+++ statistics/most_podiums_at_single_competition.rb
@@ -0,0 +1,33 @@
+require_relative "../core/statistic"
+
+class MostPodiumsAtSingleCompetition < Statistic
+  def initialize
+    @title = "Most podiums at a single competition"
+    @header = { "Podiums" => :right, "Person" => :left }
+  end
+
+  def query
+    <<-SQL
+      SELECT
+        podiums_count,
+        CONCAT('[', person.name, '](https://www.worldcubeassociation.org/persons/', person.id, ')') person,
+        CONCAT('[', competition.name, '](https://www.worldcubeassociation.org/competitions/', competition.id, '/results/podiums)') competition
+      FROM (
+        SELECT
+          COUNT(*) podiums_count,
+          personId,
+          competitionId
+        FROM Results
+        WHERE 1
+          AND roundTypeId IN ('f', 'c')
+          AND best > 0
+          AND pos IN (1, 2, 3)
+        GROUP BY personId, competitionId
+        HAVING podiums_count >= 10
+        ORDER BY podiums_count DESC
+      ) AS podiums_count_by_person_by_competition
+      JOIN Persons person ON person.id = personId AND person.subId = 1
+      JOIN Competitions competition ON competition.id = competitionId
+    SQL
+  end
+end

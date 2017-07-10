3f9215db5ffb566498ce7ef843fcf27173264d7b
Jonatan Kłosko <jonatanklosko@gmail.com>
2017-07-11 00:09:24 +0200

    Change most podiums together into a grouped statistic.

--- statistics/most_podiums_together.rb
+++ statistics/most_podiums_together.rb
@@ -1,6 +1,6 @@
-require_relative "../core/statistic"
+require_relative "../core/grouped_statistic"
 
-class MostPodiumsTogether < Statistic
+class MostPodiumsTogether < GroupedStatistic
   def initialize
     @title = "Most podiums together"
     @table_header = { "Podiums" => :right, "People" => :left }
@@ -12,7 +12,7 @@ class MostPodiumsTogether < Statistic
         GROUP_CONCAT(
           CONCAT('[', person.name, '](https://www.worldcubeassociation.org/persons/', person.id, ')')
           ORDER BY person.name
-        )
+        ) people
       FROM Results
       JOIN Persons person ON person.id = personId AND person.subId = 1
       JOIN RoundTypes round_type ON round_type.id = roundTypeId
@@ -25,18 +25,21 @@ class MostPodiumsTogether < Statistic
   end
 
   def transform(query_results)
-    query_results.each(as: :array)
-      .flatten!
-      .map! { |people| people.split(',').combination(2).to_a }
-      .flatten!(1)
-      .reduce(Hash.new(0)) do |hash, people|
-        hash[people] += 1
-        hash
-      end
-      .select { |people, podiums_together| podiums_together >= 10 }
-      .sort_by { |people, podiums_together| -podiums_together }
-      .map! do |people, podiums_together|
-        [podiums_together, people.join(" & ")]
-      end
+    podiums = query_results.map { |result| result["people"].split(',') }
+    { 2 => "Pairs", 3 => "Triples" }.map do |people_count, header|
+      people_with_podiums_together = podiums
+        .map { |people| people.combination(people_count).to_a }
+        .flatten!(1)
+        .reduce(Hash.new(0)) do |hash, people|
+          hash[people] += 1
+          hash
+        end
+        .sort_by { |people, podiums_together| -podiums_together }
+        .first(100)
+        .map! do |people, podiums_together|
+          [podiums_together, people.join(" & ")]
+        end
+      [header, people_with_podiums_together]
+    end
   end
 end

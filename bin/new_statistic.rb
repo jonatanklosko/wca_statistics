29ed672a2346cd846b9c4f049e699b23b1f09c84
Jonatan Kłosko <jonatanklosko@gmail.com>
2017-07-20 20:34:25 +0200

    Extend the new_statistic.rb script to support grouped statistics.

--- bin/new_statistic.rb
+++ bin/new_statistic.rb
@@ -2,10 +2,15 @@
 
 file_name = ARGV[0] || abort("Please provide the statistic file name.")
 class_name = file_name.split('_').map!(&:capitalize).join
+grouped_statistic = (ARGV[1] == "--grouped")
 # Read the template and make the necessary replacements.
 template_path = File.join(__dir__, "templates", "new_statistic.rb")
 template = File.read(template_path)
 template.gsub!("<<<CLASS_NAME>>>", class_name)
+if grouped_statistic
+  template.gsub!("statistic", "grouped_statistic")
+  template.gsub!("Statistic", "GroupedStatistic")
+end
 # Create a new statistic file.
 file_path = File.expand_path("../statistics/#{file_name}.rb", __dir__)
 File.write(file_path, template)

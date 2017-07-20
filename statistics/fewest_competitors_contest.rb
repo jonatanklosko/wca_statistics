06969a136a769016e0061e1c88bc98277efcb1bf
Jonatan Kłosko <jonatanklosko@gmail.com>
2017-07-20 22:39:45 +0200

    Add statistic: Fewest competitors contest.

---
+++ statistics/fewest_competitors_contest.rb
@@ -0,0 +1,26 @@
+require_relative "../core/statistic"
+
+class FewestCompetitorsContest < Statistic
+  def initialize
+    @title = "Fewest competitors contest"
+    @table_header = { "Competitors" => :right, "Competition" => :left }
+  end
+
+  def query
+    <<-SQL
+      SELECT
+        competitors_count,
+        CONCAT('[', competition.name, '](https://www.worldcubeassociation.org/competitions/', competition.id, ')') competition
+      FROM (
+        SELECT
+          COUNT(DISTINCT personId) competitors_count,
+          competitionId
+        FROM Results
+        GROUP BY competitionId
+        HAVING competitors_count <= 15
+      ) AS competitors_count_by_competition
+      JOIN Competitions competition ON competition.id = competitionId
+      ORDER BY competitors_count;
+    SQL
+  end
+end
--- statistics/index.rb
+++ statistics/index.rb
@@ -4,6 +4,7 @@ Dir["#{__dir__}/*.rb"].reject { |path| path.end_with?(__FILE__) }.each { |file| 
 STATISTICS = {
   "best_first_average" => BestFirstAverage.new,
   "dnf_rate_by_event" => DnfRateByEvent.new,
+  "fewest_competitors_contest" => FewestCompetitorsContest.new,
   "most_4th_places" => Most4thPlaces.new,
   "most_finals" => MostFinals.new,
   "most_podiums_at_single_competition" => MostPodiumsAtSingleCompetition.new,

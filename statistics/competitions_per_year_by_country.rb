5db785594495ee8c3065f7c83b5fdd7a1a2fff83
Jonatan Kłosko <jonatanklosko@gmail.com>
2017-07-22 15:49:35 +0200

    Add statistic: Competitions per year by country.

---
+++ statistics/competitions_per_year_by_country.rb
@@ -0,0 +1,36 @@
+require_relative "../core/statistic"
+
+class CompetitionsPerYearByCountry < Statistic
+  def initialize
+    @title = "Competitions per year by country"
+    @table_header = { "Competitions per year" => :right, "Competitions" => :right, "Years" => :right, "Country" => :left }
+  end
+
+  def query
+    <<-SQL
+      SELECT
+        competitions / years competitions_per_year,
+        competitions,
+        years,
+        country.name country
+      FROM (
+          SELECT
+            COUNT(DISTINCT competitionId) competitions,
+            (DATEDIFF(CURDATE(), MIN(start_date)) / 365.25) years,
+            competition.countryId country_id
+          FROM Results result
+          JOIN Competitions competition ON competition.id = competitionId
+          GROUP BY country_id
+          HAVING years >= 1
+      ) AS data_by_country
+      JOIN Countries country ON country.id = country_id
+      ORDER BY competitions_per_year DESC
+    SQL
+  end
+
+  def transform(query_results)
+    query_results.map do |result|
+      ["%0.2f" % result["competitions_per_year"], result["competitions"], "%0.2f" % result["years"], result["country"]]
+    end
+  end
+end

6434d08bcde49ad61039c7f80f430414b9375190
Jonatan Kłosko <jonatanklosko@gmail.com>
2017-07-28 20:23:37 +0200

    Get rid of multithreading because of memory problems.

--- bin/compute_all.rb
+++ bin/compute_all.rb
@@ -6,16 +6,12 @@ require_relative "../statistics/index"
 build_path = File.expand_path("../build", __dir__)
 Helpers.timed_task("Computing all statistics") do
   # Generate statistic files.
-  STATISTICS
-    .map do |statistic_id, statistic_object|
-      Thread.new do
-        markdown_result = statistic_object.markdown
-        destination_path = File.join(build_path, "#{statistic_id}.md")
-        File.write(destination_path, markdown_result)
-        puts "File generated at #{destination_path}"
-      end
-    end
-    .each(&:join)
+  STATISTICS.each do |statistic_id, statistic_object|
+    markdown_result = statistic_object.markdown
+    destination_path = File.join(build_path, "#{statistic_id}.md")
+    File.write(destination_path, markdown_result)
+    puts "File generated at #{destination_path}"
+  end
   # Build statistics index.
   list = STATISTICS.map { |statistics_id, statistic_object| "- [#{statistic_object.title}](#{statistics_id})\n" }.join
   destination_path = File.join(build_path, "README.md")

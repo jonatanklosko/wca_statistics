c74d9db26843f4c9a45c9ae3175c63a7926d3ced
Jonatan Kłosko <jonatanklosko@gmail.com>
2017-08-08 13:24:24 +0200

    Add statistic: Best medal collection from abroad by country.

---
+++ statistics/best_medal_collection_from_abroad_by_country.rb
@@ -0,0 +1,37 @@
+require_relative "../core/statistic"
+
+class BestMedalCollectionFromAbroadByCountry < Statistic
+  def initialize
+    @title = "Best medal collection from abroad by country"
+    @note = "Only medals got abroad are taken into account."
+    @table_header = { "Country" => :left, "Gold" => :center, "Silver" => :center, "Bronze" => :center, "Total" => :center }
+  end
+
+  def query
+    <<-SQL
+      SELECT
+        country.name,
+        CONCAT('**', gold_medals, '**'),
+        silver_medals,
+        bronze_medals,
+        gold_medals + silver_medals + bronze_medals total
+      FROM (
+        SELECT
+          result.countryId,
+          SUM(IF(pos = 1, 1, 0)) gold_medals,
+          SUM(IF(pos = 2, 1, 0)) silver_medals,
+          SUM(IF(pos = 3, 1, 0)) bronze_medals
+        FROM Results result
+        JOIN Competitions competition ON competition.id = competitionId
+        WHERE 1
+          AND roundTypeId IN ('c', 'f')
+          AND best > 0
+          AND competition.countryId != result.countryId
+        GROUP BY result.countryId
+      ) AS medals_by_country
+      JOIN Countries country ON country.id = countryId
+      WHERE gold_medals + silver_medals + bronze_medals > 0
+      ORDER BY gold_medals DESC, silver_medals DESC, bronze_medals DESC, country.name
+    SQL
+  end
+end

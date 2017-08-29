1a41f62bd540659d738a01a5039c1209850bac43
Jonatan Kłosko <jonatanklosko@gmail.com>
2017-08-30 00:09:31 +0200

    Fix multiple sub ids bug in Most delegated competitions.

--- statistics/most_delegated_competitions.rb
+++ statistics/most_delegated_competitions.rb
@@ -22,7 +22,7 @@ class MostDelegatedCompetitions < Statistic
         GROUP BY delegate_id
       ) AS delegated_count_by_user
       JOIN users user ON user.id = delegate_id
-      JOIN Persons person ON person.id = user.wca_id
+      JOIN Persons person ON person.id = user.wca_id AND person.subId = 1
       ORDER BY delegated_count DESC
     SQL
   end

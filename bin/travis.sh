06f3545bab502b9da3cd34af96a78a2e7676fb6f
Jonatan Kłosko <jonatanklosko@gmail.com>
2017-12-13 19:48:24 +0100

    Add the GitHub repository link in the corner of each page.

--- bin/travis.sh
+++ bin/travis.sh
@@ -15,11 +15,18 @@ else
     # Copy existing files from gh-pages to the build directory.
     git checkout gh-pages .
     mv `git ls-tree --name-only gh-pages | grep '.md'` build
-    echo "$changed_statistic_files" | while read line; do
-      echo "File has changed: $line"
-      bin/compute.rb $line
+    echo "$changed_statistic_files" | while read file; do
+      echo "File has changed: $file"
+      bin/compute.rb $file
     done
   fi
   # Update the index file in both cases.
   bin/compute_index.rb
+  # Add the GitHub repository link in the corner of each page.
+  github_repo_slug="${TRAVIS_REPO_SLUG:-jonatanklosko/wca_statistics}"
+  github_corner_template_html=`cat bin/templates/github_corner.html`
+  github_corner_html="${github_corner_template_html/"<<<GITHUB_REPO_SLUG>>>"/$github_repo_slug}"
+  grep --files-without-match "github-corner" build/* | while read file; do
+    echo -e "\n\n$github_corner_html" >> $file
+  done
 fi

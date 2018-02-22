234dd8b04fcda71780a109b2c63e07abccd89cde
Jonatan Kłosko <jonatanklosko@gmail.com>
2018-02-22 23:50:57 +0100

    Add statistic: Competition days count by region.

---
+++ statistics/competition_days_count_by_region.rb
@@ -0,0 +1,39 @@
+require_relative "../core/grouped_statistic"
+
+class CompetitionDaysCount < GroupedStatistic
+  def initialize
+    @title = "Competition days count by region"
+    @table_header = { "Days" => :right, "Region" => :left, "Competitions" => :right }
+  end
+
+  def query
+    <<-SQL
+      SELECT
+        (DATEDIFF(end_date, start_date) + 1) days,
+        country.name country,
+        continent.name continent
+      FROM Competitions
+      JOIN Countries country ON country.id = countryId
+      JOIN Continents continent ON continent.id = continentId
+    SQL
+  end
+
+  def transform(query_results)
+    {
+      "World" => ->(result) { "World" },
+      "Continents" => ->(result) { result["continent"] },
+      "Countries" => ->(result) { result["country"] }
+    }.map do |header, grouping_function|
+      results = query_results
+        .group_by(&grouping_function)
+        .map do |region, competitions|
+          days = competitions.map { |competition| competition["days"] }
+          days_mean = days.reduce(:+).to_f / competitions.count
+          [days_mean, region, competitions.count]
+        end
+        .sort_by! { |days_mean, region, competitions_count| [-days_mean, region] }
+        .map { |days_mean, region, competitions_count| ["%0.2f" % days_mean, region, competitions_count] }
+      [header, results]
+    end
+  end
+end

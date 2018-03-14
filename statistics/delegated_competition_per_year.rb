ef5252a99f90a45cbe5ab23d21203f2b0be89f10
Jonatan Kłosko <jonatanklosko@gmail.com>
2018-03-14 14:52:01 +0100

    Add statistic: Delegated competitions per year.

---
+++ statistics/delegated_competition_per_year.rb
@@ -0,0 +1,40 @@
+require_relative "../core/statistic"
+
+class DelegatedCompetitionPerYear < Statistic
+  def initialize
+    @title = "Delegated competitions per year"
+    @note = "Only delegates with at least 5 competitions are taken into account."
+    @table_header = { "Delegated per year" => :right, "Delegated" => :right, "Years" => :right, "Person" => :left, "List on WCA" => :center }
+  end
+
+  def query
+    <<-SQL
+      SELECT
+        (delegated_count / years) delegated_per_year,
+        delegated_count,
+        years,
+        CONCAT('[', person.name, '](https://www.worldcubeassociation.org/persons/', person.id, ')') person_link,
+        CONCAT('[List](https://www.worldcubeassociation.org/competitions?year=all+years&state=past&delegate=', user.id, ')') list_link
+      FROM (
+        SELECT
+          COUNT(DISTINCT competition_id) delegated_count,
+          (DATEDIFF(CURDATE(), MIN(start_date)) / 365.25) years,
+          delegate_id
+        FROM competition_delegates
+        JOIN Competitions competition ON competition.id = competition_id
+        WHERE showAtAll = 1 AND start_date < CURDATE()
+        GROUP BY delegate_id
+      ) AS delegated_count_by_user
+      JOIN users user ON user.id = delegate_id
+      JOIN Persons person ON person.id = user.wca_id AND person.subId = 1
+      WHERE delegated_count >= 5
+      ORDER BY delegated_per_year DESC
+    SQL
+  end
+
+  def transform(query_results)
+    query_results.map do |result|
+      ["%0.2f" % result["delegated_per_year"], result["delegated_count"], "%0.2f" % result["years"], result["person_link"], result["list_link"]]
+    end
+  end
+end

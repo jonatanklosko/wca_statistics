c5a4320ae16c022d2686d932e4a763afabb8b86b
Jonatan Kłosko <jonatanklosko@gmail.com>
2018-11-16 01:34:22 +0100

    Sort statistics on the index page as well.

--- bin/compute_index.rb
+++ bin/compute_index.rb
@@ -5,7 +5,7 @@ require_relative "../statistics/index"
 # Generate statistics index.
 puts "Computing statistics index."
 build_path = File.expand_path("../build", __dir__)
-list = STATISTICS.map { |statistics_id, statistic_object| "- [#{statistic_object.title}](#{statistics_id})\n" }.join
+list = STATISTICS.sort.map { |statistics_id, statistic_object| "- [#{statistic_object.title}](#{statistics_id})\n" }.join
 destination_path = File.join(build_path, "README.md")
 File.write(destination_path, list)
 puts "File generated at #{destination_path}"

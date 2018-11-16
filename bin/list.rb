ebbc58e8d100dda794ea982ec8d039dc2bffb75c
Jonatan Kłosko <jonatanklosko@gmail.com>
2018-11-16 01:06:12 +0100

    Sort statistics alphabetically.

--- bin/list.rb
+++ bin/list.rb
@@ -2,6 +2,6 @@
 
 require_relative "../statistics/index"
 
-STATISTICS.each do |statistic_id, statistic_object|
+STATISTICS.sort.each do |statistic_id, statistic_object|
   printf "%-50s %s\n", statistic_id, statistic_object.title
 end

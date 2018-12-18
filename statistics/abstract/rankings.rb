471d1a435a35cf7c0de6f80f560afe50bc9c33e2
Jonatan Kłosko <jonatanklosko@gmail.com>
2018-12-18 02:02:41 +0100

    Add an abstract Rankings statistics and reuse it for delegate and yearly rankings.

---
+++ statistics/abstract/rankings.rb
@@ -0,0 +1,49 @@
+require_relative "../../core/grouped_statistic"
+require_relative "../../core/events"
+require_relative "../../core/solve_time"
+
+class Rankings < GroupedStatistic
+  def initialize(title:, note:, condition:)
+    @condition = condition
+
+    @title = title
+    @note = note
+    @table_header = { "Person" => :left, "Result" => :right, "Country" => :left, "Competition" => :left, "Details" => :left }
+  end
+
+  def query
+    <<-SQL
+      SELECT
+        eventId event_id,
+        best single,
+        average,
+        value1, value2, value3, value4, value5,
+        CONCAT('[', person.name, '](https://www.worldcubeassociation.org/persons/', person.id, ')') person_link,
+        CONCAT('[', competition.cellName, '](https://www.worldcubeassociation.org/competitions/', competition.id, ')') competition_link,
+        country.name country
+      FROM Results
+      JOIN Persons person ON person.id = personId AND person.subId = 1
+      JOIN Countries country ON country.id = person.countryId
+      JOIN Competitions competition ON competition.id = competitionId
+      #{@condition}
+    SQL
+  end
+
+  def transform(query_results)
+    Events::ALL.flat_map do |event_id, event_name|
+      %w(single average).map do |type|
+        results = query_results
+          .select { |result| result["event_id"] == event_id && result[type] > 0 }
+          .each { |result| result[type] = SolveTime.new(event_id, type, result[type]) }
+          .sort_by! { |result| result[type] }
+          .uniq { |result| result["person_link"] }
+          .first(10)
+          .map! do |result|
+            result_details = (1..5).map { |n| SolveTime.new(event_id, :single, result["value#{n}"]).clock_format }.reject(&:empty?).join(', ')
+            [result["person_link"], "**#{result[type].clock_format}**", result["country"], result["competition_link"], result_details]
+          end
+        ["#{event_name} - #{type.capitalize}", results]
+      end
+    end
+  end
+end
--- statistics/delegates_rankings.rb
+++ statistics/delegates_rankings.rb
@@ -1,49 +1,14 @@
-require_relative "../core/grouped_statistic"
-require_relative "../core/events"
-require_relative "../core/solve_time"
+require_relative "./abstract/rankings"
 
-
-class DelegatesRankings < GroupedStatistic
+class DelegatesRankings < Rankings
   def initialize
-    @title = "Delegates rankings"
-    @note = "Only present delegates are taken into account."
-    @table_header = { "Person" => :left, "Result" => :right, "Country" => :left, "Competition" => :left, "Details" => :left }
-  end
-
-  def query
-    <<-SQL
-      SELECT
-        eventId event_id,
-        best single,
-        average,
-        value1, value2, value3, value4, value5,
-        CONCAT('[', person.name, '](https://www.worldcubeassociation.org/persons/', person.id, ')') person_link,
-        CONCAT('[', competition.cellName, '](https://www.worldcubeassociation.org/competitions/', competition.id, ')') competition_link,
-        country.name country
-      FROM Results
-      JOIN Persons person ON person.id = personId AND person.subId = 1
-      JOIN users user ON user.wca_id = personId
-      JOIN Countries country ON country.id = person.countryId
-      JOIN Competitions competition ON competition.id = competitionId
-      WHERE user.delegate_status IS NOT NULL
-    SQL
-  end
-
-  def transform(query_results)
-    Events::ALL.flat_map do |event_id, event_name|
-      %w(single average).map do |type|
-        results = query_results
-          .select { |result| result["event_id"] == event_id && result[type] > 0 }
-          .each { |result| result[type] = SolveTime.new(event_id, type, result[type]) }
-          .sort_by! { |result| result[type] }
-          .uniq { |result| result["person_link"] }
-          .first(10)
-          .map! do |result|
-            result_details = (1..5).map { |n| SolveTime.new(event_id, :single, result["value#{n}"]).clock_format }.reject(&:empty?).join(', ')
-            [result["person_link"], "**#{result[type].clock_format}**", result["country"], result["competition_link"], result_details]
-          end
-        ["#{event_name} - #{type.capitalize}", results]
-      end
-    end
+    super(
+      title: "Delegates rankings",
+      note: "Only present delegates are taken into account.",
+      condition: <<-SQL
+        JOIN users user ON user.wca_id = personId
+        WHERE user.delegate_status IS NOT NULL
+      SQL
+    )
   end
 end

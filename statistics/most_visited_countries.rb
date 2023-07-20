30be2bdce03c09a08b2d4d63977bc6d40cd8a5dd
OldManLink <oldmanlink@gmail.com>
2023-07-20 11:39:52 +0200

    Addressd PR comment

--- statistics/most_visited_countries.rb
+++ statistics/most_visited_countries.rb
@@ -20,6 +20,7 @@ class MostVisitedCountries < Statistic
         WHERE competition.countryId -- Ignore Multiple Countries used for continental FMC competitions.
           NOT IN ('XA', 'XE', 'XF', 'XM', 'XN', 'XO', 'XS', 'XW')
         GROUP BY personId
+        ORDER BY visited_countries DESC
         LIMIT 100
       ) AS visited_countries_by_person
       JOIN Persons person ON person.wca_id = personId AND person.subId = 1

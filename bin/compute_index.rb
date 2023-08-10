990abe14f5c8a88b78c0a9685f9937ce817aa589
Justin Barker <46458276+JustinTimeCuber@users.noreply.github.com>
2023-08-10 00:22:58 -0500

    hopefully fix out-of-order "average of" stats

--- bin/compute_index.rb
+++ bin/compute_index.rb
@@ -5,7 +5,16 @@ require_relative "../statistics/index"
 # Generate statistics index.
 puts "Computing statistics index."
 build_path = File.expand_path("../build", __dir__)
-list = STATISTICS.sort.map { |statistics_id, statistic_object| "- [#{statistic_object.title}](#{statistics_id})\n" }.join
+list = STATISTICS.sort{ |a, b| order(a, b) }.map { |statistics_id, statistic_object| "- [#{statistic_object.title}](#{statistics_id})\n" }.join
 destination_path = File.join(build_path, "README.md")
 File.write(destination_path, list)
 puts "File generated at #{destination_path}"
+
+def order(a, b)
+  ["average_of_"].each do |id|
+    if(a.gsub(/\d/, '') == id and b.gsub(/\d/, '') == id)
+      a.gsub(/\D/, '').to_i <=> b.gsub(/\D/, '').to_i
+    end
+  end
+  a <=> b
+end

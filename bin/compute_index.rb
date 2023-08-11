eccff105f81550465481f536b6e91d8db5cc5614
Justin Barker <46458276+JustinTimeCuber@users.noreply.github.com>
2023-08-10 21:07:57 -0500

    define function before using it (forgot how scripting languages work)

--- bin/compute_index.rb
+++ bin/compute_index.rb
@@ -2,14 +2,7 @@
 
 require_relative "../statistics/index"
 
-# Generate statistics index.
-puts "Computing statistics index."
-build_path = File.expand_path("../build", __dir__)
-list = STATISTICS.sort{ |a, b| order(a, b) }.map { |statistics_id, statistic_object| "- [#{statistic_object.title}](#{statistics_id})\n" }.join
-destination_path = File.join(build_path, "README.md")
-File.write(destination_path, list)
-puts "File generated at #{destination_path}"
-
+# Sort certain statistics numerically rather than alphabetically.
 def order(a, b)
   ["average_of_"].each do |id|
     if(a.gsub(/\d/, '') == id and b.gsub(/\d/, '') == id)
@@ -18,3 +11,11 @@ def order(a, b)
   end
   a <=> b
 end
+
+# Generate statistics index.
+puts "Computing statistics index."
+build_path = File.expand_path("../build", __dir__)
+list = STATISTICS.sort{ |a, b| order(a, b) }.map { |statistics_id, statistic_object| "- [#{statistic_object.title}](#{statistics_id})\n" }.join
+destination_path = File.join(build_path, "README.md")
+File.write(destination_path, list)
+puts "File generated at #{destination_path}"

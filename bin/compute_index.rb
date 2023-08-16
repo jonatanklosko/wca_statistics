5a67a572a1e0d60abee0d321c2ed93d37cfecd0e
Justin Barker <46458276+JustinTimeCuber@users.noreply.github.com>
2023-08-16 07:40:45 -0500

    Sort average_of_x statistics numerically instead of alphabetically (#24)
    Co-authored-by: Jonatan Kłosko <jonatanklosko@gmail.com>

--- bin/compute_index.rb
+++ bin/compute_index.rb
@@ -5,7 +5,12 @@ require_relative "../statistics/index"
 # Generate statistics index.
 puts "Computing statistics index."
 build_path = File.expand_path("../build", __dir__)
-list = STATISTICS.sort.map { |statistics_id, statistic_object| "- [#{statistic_object.title}](#{statistics_id})\n" }.join
+
+list = STATISTICS
+  .sort_by { |statistics_id, statistic_object| statistic_object.title.gsub(/\d+/) { |number| number.rjust(10, "0") } }
+  .map { |statistics_id, statistic_object| "- [#{statistic_object.title}](#{statistics_id})\n" }
+  .join
+
 destination_path = File.join(build_path, "README.md")
 File.write(destination_path, list)
 puts "File generated at #{destination_path}"

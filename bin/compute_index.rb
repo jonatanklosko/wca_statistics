0a8ee04640feac7e710509f64a3d0d0bc3aa9a63
Justin Barker <46458276+JustinTimeCuber@users.noreply.github.com>
2023-08-16 02:37:19 -0500

    Update compute_index.rb

--- bin/compute_index.rb
+++ bin/compute_index.rb
@@ -4,6 +4,7 @@ require_relative "../statistics/index"
 
 # Sort certain statistics numerically rather than alphabetically.
 def order(a, b)
+  puts "Ordering " + a.join(", ") + b.join(", ")
   ["average_of_"].each do |id|
     if(a[0].gsub(/\d/, '') == id and b[0].gsub(/\d/, '') == id)
       a[0].gsub(/\D/, '').to_i <=> b[0].gsub(/\D/, '').to_i

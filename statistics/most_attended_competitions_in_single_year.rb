34e110f49138f19c1884e7e9d102015873bce165
Justin Barker <46458276+JustinTimeCuber@users.noreply.github.com>
2024-03-27 17:51:15 -0400

    Another possible fix

--- statistics/most_attended_competitions_in_single_year.rb
+++ statistics/most_attended_competitions_in_single_year.rb
@@ -8,7 +8,7 @@ class MostAttendedCompetitionsInSingleYear < Statistic
 
   def query
     <<-SQL
-      SELECT
+      SELECT /*+ SET_VAR(group-concat-max-len=16384) */
         attended_within_year,
         CONCAT('[', person.name, '](https://www.worldcubeassociation.org/persons/', person.wca_id, ')') person_link,
         competitions_year,
